1a0582006e8013c9163eb084bdeefaa048248816
Martin Franck <franck.martin12@hotmail.com>
2012-11-21 17:07:45 +0100

    Les commande en *.do

--- src/java/command/CommandManager.java
+++ src/java/command/CommandManager.java
@@ -7,7 +7,7 @@ public class CommandManager {
     protected static HashMap<String, Command> cmds = new HashMap<String, Command>();
 
     public static void init() {
-        cmds.put("default", new DefaultCommand());
+        cmds.put("acc", new DefaultCommand());
         cmds.put("error", new ErrorCommand());
         cmds.put("test", new TestCommand());
     }
--- src/java/servlet/FrontController.java
+++ src/java/servlet/FrontController.java
@@ -3,29 +3,32 @@ package servlet;
 import command.Command;
 import command.CommandManager;
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(name = "serv", urlPatterns = {"/"})
+@WebServlet(name = "serv", urlPatterns = {"*.do"})
 public class FrontController extends HttpServlet {
 
-    private final String TEMPLATE_SERVLET = "/WEB-INF/templates/albumphoto.jsp";
+    private final String TEMPLATE_SERVLET = "/WEB-INF/layouts/albumphoto.jsp";
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        String commandUrl = request.getRequestURI().replace(getServletContext().getContextPath() + "/", "");
+        String commandUrl = null;
+        Pattern p = Pattern.compile(".*/(.*).do");
+        Matcher m = p.matcher(request.getRequestURI());
+        if (m.find() && m.groupCount() == 1) {
+            commandUrl = m.group(1);
+        }
         Command cmd = CommandManager.getCommand(commandUrl);
-        if (!commandUrl.equals("")) {
-            if (cmd == null) {
-                cmd = CommandManager.getCommand("error");
-            }
-        } else {
-            cmd = CommandManager.getCommand("default");
+        if (cmd == null) {
+            cmd = CommandManager.getCommand("error");
         }
         cmd.actionPerform(request);
         getServletContext().getRequestDispatcher(TEMPLATE_SERVLET).forward(request, response);
